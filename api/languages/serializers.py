f0627a0622308c5a4fa814d5b653095c445aa375
swa <swa@swa-asus-vivobook.aircard>
2019-07-03 21:55:14 +0200

    feat: change serializer to Hyperlink and add urls to serialized models

--- api/languages/serializers.py
+++ api/languages/serializers.py
@@ -2,11 +2,12 @@ from rest_framework import serializers
 
 from .models import Language
 
-class LanguageSerializer(serializers.ModelSerializer):
+class LanguageSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = Language
 		fields = (
 			'id',
+			'url',
 			'name',
 			'paradigm',
 			)
